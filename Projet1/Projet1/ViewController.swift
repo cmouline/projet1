608611f5bd9ef4a0b00fc733f3ba9377bf1382b1
Moulinet Chloë <cmoulinet@gmail.com>
2020-07-19 17:36:11 +0200

    Add print in viewWillAppear

--- Projet1/Projet1/ViewController.swift
+++ Projet1/Projet1/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        print("DidAppear")
     }
 }
 
